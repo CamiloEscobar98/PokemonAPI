e1f20ad05c55a37f4826d6425a56e9286e9610be
Andrés Camilo Yáñez Escobar <andrescamiloye@ufps.edu.co>
2022-02-05 12:47:10 -0500

    Process: Modifying UserFactory for make email users unique and with them fullnames. | Action: Modify

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 class UserFactory extends Factory
 {
@@ -26,18 +27,21 @@ class UserFactory extends Factory
             '310', '311', '312', '313', '314', '320', '321', '322', '323',
             '315', '316', '317', '318', '319', '350', '351',
         );
-
         $random_number = rand(0, sizeof($phone_prefix) - 1);
-
         $phone = $phone_prefix[$random_number] . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9);
 
         $random_gender = rand(0, 1) ? 'male' : 'female';
 
         $firstname = $this->faker->name($random_gender);
         $fullname = $firstname . ' ' . $this->faker->lastName();
+
+        $email_domains = array('gmail', 'outlook', 'hotmail');
+        $random_email_domain = rand(0, sizeof($email_domains) - 1);
+        $email = str_replace(' ', '', strtolower($fullname)) . '@' . $email_domains[$random_email_domain] . '.com';
+
         return [
             'fullname' => $fullname,
-            'email' => $this->faker->unique()->safeEmail,
+            'email' => $email,
             'phone' => $phone,
             'birth_date' => $this->faker->dateTimeBetween('-40 years', '-18 years'),
             'nickname' => str_replace(' ', '', strtolower($firstname)) . rand(101, 999),
