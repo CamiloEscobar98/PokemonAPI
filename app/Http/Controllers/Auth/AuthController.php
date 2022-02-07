09b011eaa201a363975f353fa2393c2d0566834c
Andrés Camilo Yáñez Escobar <andrescamiloye@ufps.edu.co>
2022-02-07 12:38:23 -0500

    Process: Logout endpoint created and login endpoint modified | Action: Create, Modifyu

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -11,25 +11,6 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-    {
-        $this->validate($request, [
-            'nickname' => 'required',
-            'password' => 'required'
-        ]);
-
-        $credentials = $request->only(['nickname', 'password']);
-
-        $user = User::where('nickname', $credentials['nickname'])->first();
-        if (Hash::check($credentials['password'], $user->password)) {
-            $apikey = base64_encode(Str::random(40));
-            User::where('nickname', $credentials['nickname'])->update(['remember_token' => "$apikey"]);;
-            return response()->json(['status' => 'success', 'auth_token' => $apikey]);
-        } else {
-            return response()->json(['status' => 'fail'], 401);
-        }
-    }
-
     public function register(Request $request)
     {
         $rules = [
@@ -61,6 +42,48 @@ class AuthController extends Controller
             return response()->json(['status' => 'fail', 'message' => 'User not created']);
         }
     }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'nickname' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only(['nickname', 'password']);
+        $user = User::where('nickname', $credentials['nickname'])->first();
+
+        if (!empty($user)) {
+            if (Hash::check($credentials['password'], $user->password)) {
+                $apikey = base64_encode(Str::random(40));
+                User::where('nickname', $credentials['nickname'])->update(['remember_token' => "$apikey"]);;
+                return response()->json(['status' => 'success', 'auth_token' => $apikey]);
+            } else {
+                return response()->json(['status' => 'fail', 'message' => 'Unauthorized'], 401);
+            }
+        } else {
+            return response()->json(['status' => 'fail', 'message' => 'User Not Found'], 404);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $rememberToken = $request->header('Authorization');
+        $user = User::where('remember_token', $rememberToken)->first();
+        if (!empty($user)) {
+            $user->remember_token = null;
+            $isLogout =  $user->save();
+            if ($isLogout) {
+                return response()->json(['status' => 'success'], 200);
+            } else {
+                return response()->json(['status' => 'fail'], 500);
+            }
+        } else {
+            return response()->json(['status' => 'fail', 'message' => 'User not found']);
+        }
+    }
+
+
 
     public function profile(Request $request)
     {
--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,15 @@ $router->get('/', function () use ($router) {
     return response('Welcome to Test IPT API V1.0');
 });
 
-$router->post('login', 'Auth\AuthController@login');
 $router->post('register', 'Auth\AuthController@register');
+
+$router->post('login', 'Auth\AuthController@login');
 
 
 $router->group(['middleware' => 'auth'], function () use ($router) {
     $router->get('profile', 'Auth\AuthController@profile');
     $router->put('profile', 'Auth\AuthController@update');
+    $router->post('logout', 'Auth\AuthController@logout');
 
     $router->get('users', 'UserController@index');
     $router->get('users/{uuid}', 'UserController@show');
